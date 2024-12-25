0c5462725fc58c3aef9d302d5f249b0cd8e4bee0
jennifershi255 <jennifershi255@gmail.com>
2024-12-24 19:52:41 -0500

    fixing responsive banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -71,48 +71,51 @@ export const Banner = () => {
 
   return (
     <section className="banner" id="home">
-      <Container>
-        <Row
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            width: "100%",
-          }}
-        >
-          <Col xs={12} md={6} xl={7}>
-            <div
-              style={{ minHeight: "5px", marginBottom: 25 }}
-              className="tagline"
-            >
-              <span>{text}</span>
-              {text === "" && (
-                <span style={{ visibility: "hidden" }}>&nbsp;</span>
-              )}{" "}
-              {/* Invisible span */}
-            </div>
+      <div className="banner-content">
+        <Container>
+          <Row
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              width: "100%",
+            }}
+          >
+            <Col xs={12} md={6} xl={7}>
+              <div
+                style={{ minHeight: "5px", marginBottom: 25 }}
+                className="tagline"
+              >
+                <span>{text}</span>
+                {text === "" && (
+                  <span style={{ visibility: "hidden" }}>&nbsp;</span>
+                )}{" "}
+                {/* Invisible span */}
+              </div>
 
-            <TrackVisibility>
-              {({ isVisible }) => (
-                <div
-                  className={
-                    isVisible ? "animate__animated animate__zoomIn" : ""
-                  }
-                >
-                  <h1 className="namecard"> Jennifer Shi</h1>
-                </div>
-              )}
-            </TrackVisibility>
-            <p style={{ marginTop: 20 }}>
-              Computer Science & Finance <span className="purple">@</span>{" "}
-              University of Waterloo
-            </p>
-            <button className="button" onClick={scrollToAbout}>
-              About Me <ArrowRightCircle style={{ color: "white" }} size={25} />
-            </button>
-          </Col>
-        </Row>
-      </Container>
+              <TrackVisibility>
+                {({ isVisible }) => (
+                  <div
+                    className={
+                      isVisible ? "animate__animated animate__zoomIn" : ""
+                    }
+                  >
+                    <h1 className="namecard"> Jennifer Shi</h1>
+                  </div>
+                )}
+              </TrackVisibility>
+              <p style={{ marginTop: 20 }}>
+                Computer Science & Finance <span className="purple">@</span>{" "}
+                University of Waterloo
+              </p>
+              <button className="button" onClick={scrollToAbout}>
+                About Me{" "}
+                <ArrowRightCircle style={{ color: "white" }} size={25} />
+              </button>
+            </Col>
+          </Row>
+        </Container>
+      </div>
     </section>
   );
 };
